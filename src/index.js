e0c1ca993e01278eb095b0cb6c287d1564459e84
AlihanAimaganbetov <126770656+AlihanAimaganbetov@users.noreply.github.com>
2023-12-15 13:10:51 +0600

    second

--- src/index.js
+++ src/index.js
@@ -2,17 +2,33 @@ import React from 'react';
 import ReactDOMClient from 'react-dom/client';
 import './index.css';
 import Gallery from './components/Gallery';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ImagePage from './components/ImagePage'; // ваш компонент ImagePage
 
-function App() {
-    console.log("ss");
-    return (
-        <div className="App">
-        <h1>Фотогалерея</h1>
-        <Gallery imagesPerPage={24}/>
-        </div>
-    );
+const App = () => {
   
-}
+    const imagesPerPage = 25; // количество изображений на странице
+    return (
+        <Router>
+            <Routes>
+                {/* Маршрут для компоненты Gallery */}
+                <Route
+                    exact
+                    path="/"
+                    element={<Gallery imagesPerPage={imagesPerPage} />}
+                />
+
+
+
+                {/* Маршрут для компоненты ImagePage */}
+                <Route path="/image-page/:imageUrl" element={<ImagePage />} />
+
+
+                {/* Другие маршруты, если есть */}
+            </Routes>
+        </Router>
+    );
+};
 console.log("ww");
 
 const app = ReactDOMClient.createRoot(document.getElementById("root"))
